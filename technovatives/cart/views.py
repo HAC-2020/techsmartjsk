2a1c81a5bc68f511b3b3dfd73d9898189ecccc2a
Jaskirat Singh <techsmartjsk@gmail.com>
2020-08-09 00:46:06 +0530

    Done

--- technovatives/cart/views.py
+++ technovatives/cart/views.py
@@ -1,22 +1,31 @@
 from django.shortcuts import render
 from cart.models import cart_items
 from django.http import HttpResponseRedirect
+
 #Cart Items
 def add_to_cart(request):
-    if request.method == 'POST':
-        try:
-            newitem = cart_items(name = request.POST['name'],
-                               price = request.POST['price'], 
-                               img = request.POST['img'])    
-            newitem.save()
-        except:
-            print('Error')    
+    newitem = cart_items(name = request.POST['name'],
+                    price = request.POST['price'], 
+                    img = request.POST['img'])    
+    newitem.save()   
     return HttpResponseRedirect('/') 
 
 def cart_all_items(request):
     all_items = cart_items.objects.all()
+    total = 0
+    tax = 0
+    for c in all_items:
+        total += int(c.price)
+        tax = 0.05 * total
+    total_amount = total + tax    
     return render(request,'cart.html',
     {
-        'cart_all' : all_items
+        'cart_all' : all_items,
+        'total': total_amount,
+        'tax': tax,
     })
 
+def delete_cart_item(request,item_id):
+    item_to_delete = cart_items.objects.get(id=item_id)
+    item_to_delete.delete()
+    return HttpResponseRedirect('/cart/')
