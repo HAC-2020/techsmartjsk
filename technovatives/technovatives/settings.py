4ba303119cbda8fecd9063cdbaa387207350552c
techsmartjsk <techsmartjsk@gmail.com>
2020-08-08 11:08:00 +0530

    Done

--- technovatives/technovatives/settings.py
+++ technovatives/technovatives/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'mfnsoi^0o-cyojm68xis1pz4&gp1za_l*s!$leaku4_o+xo_#*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['technovatives.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['technovatives.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
